ff2f47c8df6762fa148826821c6288649a5349b1
AlexStan0 <stan_alexandru06@yahoo.com>
2024-02-10 14:48:15 -0500

    created the drive train code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,13 +6,9 @@ package frc.robot;
 
 import frc.robot.Constants.MotorConstants;
 import frc.robot.Constants.OperatorConstants;
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -21,38 +17,15 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  
-  //define the motors and their device ID + motor type for the left motors
-  private final CANSparkMax m_leftMotor_front = new CANSparkMax(MotorConstants.kLMotorFront, MotorType.kBrushless);
-  private final CANSparkMax m_leftMotor_rear = new CANSparkMax(MotorConstants.kLMotorRear, MotorType.kBrushless);
 
-  //generate the motor group for left motors
-  private final MotorControllerGroup mgroup_left = new MotorControllerGroup(m_leftMotor_front, m_leftMotor_rear);
-
-  //define the motors and their device ID + motor type for the right motors
-  private final CANSparkMax m_rightMotor_front = new CANSparkMax(MotorConstants.kRMotorFront, MotorType.kBrushless);
-  private final CANSparkMax m_rightMotor_rear = new CANSparkMax(MotorConstants.kRMotorRear, MotorType.kBrushless);
-
-  //generate the motor group for the right motors
-  private final MotorControllerGroup mgroup_right = new MotorControllerGroup(m_rightMotor_front, m_rightMotor_rear);
-
-  //generate the differential drive 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(mgroup_right::set, mgroup_left::set);
-
-  //define the controller (PS4) and a new drive stick
-  private final CommandXboxController drive_controller = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   public Robot() {
-    SendableRegistry.addChild(m_robotDrive, mgroup_left);
-    SendableRegistry.addChild(m_robotDrive, mgroup_right);
+    //SendableRegistry.addChild(m_robotDrive, mgroup_left);
+    //SendableRegistry.addChild(m_robotDrive, mgroup_right);
   }
 
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    mgroup_right.setInverted(true);
   }
 
   @Override
@@ -60,8 +33,6 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    // m_robotDrive.arcadeDrive(-drive_controller.getLeftY(), -drive_controller.getLeftX());
-    m_robotDrive.tankDrive(-drive_controller.getLeftY(), -drive_controller.getRightY());
 
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.MotorConstants;
+import frc.robot.Constants.OperatorConstants;
+
+public class Drive extends SubsystemBase {
+
+  //define the motors and their device ID + motor type for the left motors
+  private final CANSparkMax m_left_leader = new CANSparkMax(MotorConstants.kLMotorFront, MotorType.kBrushless);
+  private final CANSparkMax m_left_follower = new CANSparkMax(MotorConstants.kLMotorRear, MotorType.kBrushless);
+
+  //define the motors and their device ID + motor type for the right motors
+  private final CANSparkMax m_right_leader = new CANSparkMax(MotorConstants.kRMotorFront, MotorType.kBrushless);
+  private final CANSparkMax m_right_follower = new CANSparkMax(MotorConstants.kRMotorRear, MotorType.kBrushless);
+
+  //generate the differential drive 
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left_leader, m_right_leader);
+
+  /** Creates a new Drive. */
+  public Drive() {
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_left_leader.restoreFactoryDefaults();
+    m_left_leader.setIdleMode(IdleMode.kBrake);
+    m_left_leader.burnFlash();
+    
+    m_left_follower.restoreFactoryDefaults();
+    m_left_follower.setIdleMode(IdleMode.kBrake);
+    m_left_follower.follow(m_left_leader);
+    m_left_follower.burnFlash();
+        
+    m_right_leader.restoreFactoryDefaults();
+    m_right_leader.setIdleMode(IdleMode.kBrake);
+    m_right_leader.burnFlash();
+        
+    m_right_follower.restoreFactoryDefaults();
+    m_right_follower.setIdleMode(IdleMode.kBrake);
+    m_right_follower.follow(m_right_leader);
+    m_right_follower.burnFlash();
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void teleopDrive(double x, double y) {
+
+    m_robotDrive.arcadeDrive(x, y);
+    
+  }
+
+}
