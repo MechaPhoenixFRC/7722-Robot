09aeef9fb5c00054f2df47cf1f449f09eee66d6c
TwrblxDevs <156226057+TwrblxDevs@users.noreply.github.com>
2024-02-29 15:14:48 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,8 @@ public final class Constants {
 
     // public static final int FireButton = 0;
 
-    public static final double ShootTimePower = 0.30;
+    public static final double RevShootTimePower = 0.30;
+    public static final double ShootTimePower = 0.27;
   }
 
 
